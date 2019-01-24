3c7d0ec9f629e79758787cb14f481b0ca141b496
Juan <35646489+beingwithu@users.noreply.github.com>
2019-01-24 07:32:55 -0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,134 +1,105 @@
 module.exports.run = (client, message, args) => {
-
     const config = require("../config.json");
     const sysop = require("../sysop.js");
-    if ((!args[0]) || (args[1] && !Number(args[1]) || args[2])) return message.reply(`use    \`${config.prefix}mute @user <optional: time>\``);
-
-    if (!message.mentions.members.first()) return message.reply("provide a @user to mute");
-    const sender = message.author,
-        mSender = message.member;
     const target = message.mentions.members.first();
 
-    let muted;
-    let time = 300000;
-    let minutes, seconds;
 
-
-    		function setChannelPerms(muted) {
-
-    			message.guild.channels.forEach(async (channel, id) => { // disable these perms for every existing channel
-    				await channel.overwritePermissions(muted, {
-    					KICK_MEMBERS: false,
-    					BAN_MEMBERS: false,
-    					MANAGE_CHANNELS: false,
-    					MANAGE_GUILD: false,
-    					ADD_REACTIONS: false,
-    					SEND_MESSAGES: false,
-    					MANAGE_MESSAGES: false,
-    					ATTACH_FILES: false,
-    					CONNECT: false,
-    					MUTE_MEMBERS: false,
-    					MANAGE_ROLES: false
-    				});
-    			});
-    			return undefined;
-    		}
-
-
-    /* unmute */
-    function unmute() {
-
-        function nowUnmute() {
-
-
-            if (target.roles.find(val => val.name === "muted")) {
-
-                muted = target.roles.find(val => val.name === "muted");
-
-                target.removeRole(muted)
-                    .then(console.log(`${target.user.username}\s mute is over\n`))
-                    .then(message.channel.send(`${target.user.username}\s mute is over`))
-                    .catch(console.error);
-
-                return undefined;
-            }
-        }
-
-        nowUnmute();
+	if (sysop.check(message.author.id) === false && !message.member.hasPermission("MANAGE_ROLES")) {
+    	return message.reply("you must be \`Leader\` to use this command");
     }
 
-    /* mute */
-    function mute() {
-
-        const sys = [sysop.isSysop(sender.id), sysop.ofSysop(target.id)];
-
-        if (sys[0] === false && !mSender.hasPermission("MANAGE_NICKNAMES")) return message.reply("you must be \`Leader\` or higher to use this command");
-
-        if (sys[1] === true || target.hasPermission("MANAGE_NICKNAMES")) {
-            console.log(`\n*** ${sender.username} tried to mute ${target.user.username} ***`);
-            return message.reply("a mute should never be used on another leader. If you feel like this person should be muted, contact King Penguin (Flareon) or marlboro (Glaceon)");
-        }
-
-        function createMute() { /* create the muted role, if not already done */
-
-            message.guild.createRole({
-                    name: "muted",
-                    permissions: ["READ_MESSAGE_HISTORY"],
-                    mentionable: true
-                })
-                .then(console.log("muted role has been made"))
-                .then(message.reply("the role was just now created. Run this command again"))
-                .catch(console.error);
-
-
-			setChannelPerms(message.guild.roles.find(val => val.name === "muted"));
-            return undefined;
-        }
-
-
-        function defineMute() { /* define the muted role */
-
-            if (message.guild.roles.find(val => val.name === "muted")) muted = message.guild.roles.find(val => val.name === "muted");
-
-        }
-
-
-        function defineTime() { /* define the duration of time */
-
-            if (Number(args[1])) time = args[1] * 60000;
-            minutes = time / 60000;
-            seconds = minutes * 100;
-
-        }
-
-
-        function nowMute() {
-
-            /* checks if person is already muted */
-            if (target.roles.find(val => val.name === "muted")) return message.reply(`${target.user.username} is already muted`); /* checks if person is already muted */
-
-            target.addRole(muted);
-
-            /* set time of duration */
-            message.delete();
-            if (minutes < 1) message.channel.send(`${target.user.username} was muted for ${seconds} seconds`);
-            else message.channel.send(`${target.user.username} was muted for ${minutes} minutes`);
-
-            setTimeout(unmute, time); /* calls unmute function in the specified time */
-        }
-
-
-        if (!message.guild.roles.find(val => val.name === "muted")) /* check if muted role exists */
-            createMute();
-
-        if (!muted) defineMute();
-        defineTime();
-        if (Number(time) && muted) nowMute();
-        setChannelPerms(message.guild.roles.find(val => val.name === "muted"));
-
+	if (!args[0] || args[3]) {
+    	return message.reply(`use    \`${config.prefix}mute @user <optional: time>\``);
     }
-    mute();
+
+	if (args[0] === "setup-config" && args[1] !== "true") {
+    	return message.reply(`the setup-config command is    \`${config.prefix}mute setup-config true\``);
+    }
+
+    if (args[0] !== "setup-config" && !target) {
+    	return message.reply("provide a \`@user\` to mute");
+    }
+
+    if ((args[0] !== "setup-config") && (target && sysop.check(target.id) === true || target.hasPermission("MANAGE_ROLES"))) {
+    	return message.reply(`you can not mute another \`Moderator\`. If you feel like this user should be muted, use the \`${config.prefix}force mute\` command or contact the server \`Administrator\`.`);
+    }
+
+    if ((args[0] !== "setup-config") && (target && target.roles.find(val => val.name === "muted"))) {
+    	return message.reply(`${target.user.username} is already muted.`);
+    }
+
+	if (Number(args[1]) && Number(args[1]) < 5) {
+		return message.reply("the mute duration should be at least 5 minutes. Leave this part empty for a default timer of 5 minutes.");
+	}
+
+
+
+    function setChannelPerms(state) {
+    	const muted = message.guild.roles.find(val => val.name === "muted");
+		if (state !== "true") { return message.reply(`\`${config.prefix}mute setup-config true\``); }
+
+    	message.guild.channels.forEach(async (channel, id) => {
+			await channel.overwritePermissions(muted, {
+				SEND_MESSAGES: false,
+				MANAGE_MESSAGES: false,
+				ATTACH_FILES: false,
+				CONNECT: false,
+			})
+			.catch(console.error);
+		});
+		return message.channel.send("Done setting up the configurations! You may now use the \`mute\` command.");
+	}
+
+
+	function unmute(target, muted) {
+		target.removeRole(muted).catch(console.error);
+		return message.channel.send(`$<@{target.user.id}> can now chat again!`);
+	}
+
+
+	function mute(target, muted, time) {
+		target.addRole(muted).catch(console.error);
+		message.channel.send(`${target.user.username} has been muted for ${time / 60000} minutes`);
+	}
+
+
+	function createMute() {
+		if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+			return message.channel.send("Seems like I don't have the permission (manage roles) to do so :cry:");
+		}
+		message.guild.createRole({
+			name: "muted",
+			permissions: ["READ_MESSAGE_HISTORY"],
+			mentionable: true
+		})
+		.catch(console.error);
+
+		message.channel.send(`All done! Try doing \`${config.prefix}mute setup-config true\``);
+	}
+
+
+	function main() {
+		let time = 5 * 60000;
+		let muted_role;
+
+		if (Number(args[1])) { time = args[1] * 60000; }
+
+		if (args[0] === "setup-config") {
+			setChannelPerms(args[1])
+		}
+		if (!message.guild.roles.find(val => val.name === "muted")) {
+			message.channel.send("Oops, seems like there is no \`muted\` role for this server. Allow me to make one for you!");
+			createMute();
+		}
+		else if (target) {
+			muted_role = message.guild.roles.find(val => val.name === "muted");
+			mute(target, muted_role, time);
+			setTimeout(unmute, time, target, muted_role);
+		}
+	}
+	main();
 };
+
 
 exports.conf = {
     aliases: []
