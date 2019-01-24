13e203b1a47415cb4a844117fac6e71106fdcfe3
Juan <35646489+beingwithu@users.noreply.github.com>
2019-01-24 07:57:39 -0500

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,17 +2,20 @@ module.exports.run = (client, message, args) => {
 
     const config = require("../config.json");
     const sysop = require("../sysop.js");
-    if (args[0]) return message.reply(`use    \`${config.prefix}test\``);
-    const sender = message.author;
+
+    if (sysop.check(message.author.id) === false) {
+        return message.reply("this is a \`dev-only\` command for testing purposes only!");
+    }
+    if (args[0]) {
+    	return message.reply(`use    \`${config.prefix}test\``);
+    }
+
 
 
     function test() {
-
-        const sys = sysop.isSysop(sender.id);
-
-        if (sys === false) return message.reply("this is a \`dev-only\` command for testing purposes only!");
         return message.reply("no other duelist can compare to you, you're the smartest and best-looking duelist in the planet, perfect in every way shape and form.");
     }
+
     test();
 };
 
